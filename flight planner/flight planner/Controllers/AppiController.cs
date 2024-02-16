4b8f904c3f670a5d1297537dd5abca3c2bba6f78
GA0909 <gatisabramovics@gmail.com>
2024-02-16 12:25:59 +0200

    adding files

---
+++ flight planner/flight planner/Controllers/AppiController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace flight_planner.Controllers
+{
+    [Authorize]
+    [Route("admin-api")]
+    [ApiController]
+    public class AppiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("flights/{id}")]
+        public IActionResult GetFlight(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ flight planner/flight planner/d/verificationHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+
+namespace flight_planner.d
+{
+    public class verificationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public verificationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
+        {
+        }
+
+        public verificationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder) : base(options, logger, encoder)
+        {
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            var endpoint = Context.GetEndpoint();
+            if (endpoint?.Metadata?.GetMetadata<IAllowAnonymous>() != null)
+                return AuthenticateResult.NoResult();
+
+            if (!Request.Headers.ContainsKey("Authorization"))
+                return AuthenticateResult.Fail("Missing Authorization Header");
+            var isAuthenticated = false;
+            try
+            {
+                if (username == "codelex-admin" && pasword == "Password123")
+                {
+                    isAuthenticated = true;
+                }
+            }
+        }
+    }
+}
