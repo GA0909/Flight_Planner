52734acda1b5707459ffb86253df4ef879807ace
GA0909 <gatisabramovics@gmail.com>
2024-03-14 13:11:25 +0200

    adding files

---
+++ FlightPlannerV4/FlightPlanner/Program.cs
@@ -0,0 +1,61 @@
+
+using FlightPlanner.Core.Services;
+using FlightPlanner.Data;
+using FlightPlanner.HANDLES;
+using FlightPlanner.models;
+using FlightPlanner.Service;
+using FluentValidation;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace FlightPlanner
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddAuthentication("BasicAuthentication")
+                .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", null);
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContextPool<FlightPlannerDbContext>(option =>
+            {
+                option.UseSqlServer(builder.Configuration.GetConnectionString("flight-planner"));
+            });
+            builder.Services.AddTransient<IFlightPlannerDbContext, FlightPlannerDbContext>();
+            builder.Services.AddTransient<IDbService,DbService>();
+            builder.Services.AddTransient<IEntityService<Airport>, EntityService<Airport>>();
+            builder.Services.AddTransient<IEntityService<Flight>, EntityService<Flight>>();
+
+            builder.Services.AddTransient<IFlightService, FlightService>();
+
+            builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ FlightPlannerV4/FlightPlanner/Validations/AirportViewModelValidator.cs
@@ -0,0 +1,16 @@
+﻿using FlightPlanner.models;
+using FluentValidation;
+
+namespace FlightPlanner.Validations
+{
+    public class AirportViewModelValidator : AbstractValidator<AirportViewModel>
+    {
+        public AirportViewModelValidator() 
+        {
+            RuleFor(viewModel => viewModel.Airport).NotEmpty();
+            RuleFor(viewModel => viewModel.City).NotEmpty();
+            RuleFor(viewModel => viewModel.Country).NotEmpty();
+        }
+
+    }
+}
