0cfada154420389b1caf342c9d64d84f2d717aab
GA0909 <gatisabramovics@gmail.com>
2024-03-14 14:05:17 +0200

    Restarting again

---
+++ FlightPlannerV4/FlightPlanner.Core/Services/IFlightService.cs
@@ -0,0 +1,17 @@
+﻿using FlightPlanner.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightPlanner.Core.Services
+{
+    public interface IFlightService : IEntityService<Flight>
+    {
+        Flight? GetFullFlighById(int id);
+        bool FlightExists(Flight flight);
+        PageResult GetMatchedFlights(SearchFlightsRequest req);
+
+    }
+}
